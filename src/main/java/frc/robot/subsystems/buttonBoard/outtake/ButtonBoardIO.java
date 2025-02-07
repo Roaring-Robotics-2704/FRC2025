956e7d1444a29aec0f972f70af01b642930834fa
nogo721 <yt.nogo721.pro@gmail.com>
2025-02-06 19:35:57 -0600

    temp update

---
+++ src/main/java/frc/robot/subsystems/buttonBoard/outtake/ButtonBoardIO.java
@@ -0,0 +1,19 @@
+package frc.robot.subsystems.outtake;
+
+import org.littletonrobotics.junction.AutoLog;
+
+                                               //Sets up outtake variables
+public interface OuttakeIO {
+    @AutoLog
+    class OuttakeIOInputs {
+        public boolean connected = false;
+        public double velocityRadPerSec = 0.0;
+        public double voltage = 0.0;
+        public double currentAmps = 0.0;
+        public boolean outtakeLoaded= false;
+    }
+
+    default void setSpeed(double speed) {}
+
+    default void updateInputs(OuttakeIOInputs inputs) {}
+}
