7010f95d6d355e2e8a3f53fab01b89428e864abc
Nova <yt.nogo721.pro@gmail.com>
2025-02-08 15:29:10 -0600

    BuBo interface made
    Created initial interface between data sent by ButtonBoard and predefined Reef and Branch classes

--- src/main/java/frc/robot/subsystems/buttonBoard/ButtonBoard.java
+++ src/main/java/frc/robot/subsystems/buttonBoard/ButtonBoard.java
@@ -7,19 +7,71 @@ package frc.robot.subsystems.buttonBoard;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.auto.reef.Reef;
+import frc.robot.auto.reef.Branch.Level;
 import frc.robot.auto.reef.Reef.Faces;
+import frc.robot.auto.reef.Branch.Side;
+
 
 public class ButtonBoard extends SubsystemBase {
   Reef reef;
-  GenericHID board = new GenericHID(ButtonBoardConstants.BUTTON_BOARD_PORT);
+  Level currentLevel = Level.L3;
+  GenericHID board = new GenericHID(ButtonBoardConstants.BB_PORT);
   /** Creates a new ButtonBoard. */
   public ButtonBoard(Reef reef) {
     this.reef = reef;
   }
 
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    reef.getReefSide(Faces.FRONT).setSelected(true);
+    
+    reef.getReefSide(Faces.FRONT).setSelected(board.getRawButton(ButtonBoardConstants.REEF_NEAR_CENTER));
+    reef.getReefSide(Faces.FRONT_LEFT).setSelected(board.getRawButton(ButtonBoardConstants.REEF_NEAR_LEFT));
+    reef.getReefSide(Faces.FRONT_RIGHT).setSelected(board.getRawButton(ButtonBoardConstants.REEF_NEAR_RIGHT));
+    reef.getReefSide(Faces.BACK_LEFT).setSelected(board.getRawButton(ButtonBoardConstants.REEF_FAR_LEFT));
+    reef.getReefSide(Faces.BACK_RIGHT).setSelected(board.getRawButton(ButtonBoardConstants.REEF_FAR_RIGHT));
+    reef.getReefSide(Faces.BACK).setSelected(board.getRawButton(ButtonBoardConstants.REEF_FAR_CENTER));
+
+    if(board.getRawButton(ButtonBoardConstants.LEVEL_4R)) {
+      currentLevel = Level.L4;
+    }
+    else if(board.getRawButton(ButtonBoardConstants.LEVEL_4L)) {
+      currentLevel = Level.L4;
+    }
+    else if(board.getRawButton(ButtonBoardConstants.LEVEL_3R)) {
+      currentLevel = Level.L3;
+    }
+    else if(board.getRawButton(ButtonBoardConstants.LEVEL_3L)) {
+      currentLevel = Level.L3;
+    }
+    else if(board.getRawButton(ButtonBoardConstants.LEVEL_2R)) {
+      currentLevel = Level.L2;
+    }
+    else if(board.getRawButton(ButtonBoardConstants.LEVEL_2L)) {
+      currentLevel = Level.L2;
+    }
+    else if(board.getRawButton(ButtonBoardConstants.LEVEL_1R)) {
+      currentLevel = Level.L1;
+    }
+    else if(board.getRawButton(ButtonBoardConstants.LEVEL_1L)) {
+      currentLevel = Level.L1;
+    }
   }
+
+  public Side getSelectedBranchSide() {
+    if(ButtonBoardConstants.LEVEL_1L==1 || ButtonBoardConstants.LEVEL_2L==1 || ButtonBoardConstants.LEVEL_3L==1 || ButtonBoardConstants.LEVEL_4L==1) {
+      return Side.LEFT;
+    }
+    else {
+      return Side.RIGHT;
+    }
+
+  }
+
+
+  public Level getSelectedLevel() {
+    return currentLevel;
+  }
+  
 }
--- src/main/java/frc/robot/subsystems/buttonBoard/ButtonBoardConstants.java
+++ src/main/java/frc/robot/subsystems/buttonBoard/ButtonBoardConstants.java
@@ -8,24 +8,25 @@ package frc.robot.subsystems.buttonBoard;
 public class ButtonBoardConstants {
 	private ButtonBoardConstants() {
 	}
-	public static final int BUTTON_BOARD_PORT = 1;
+	public static final int BB_PORT = 1;
 	
-	public static final int IDstuffs;
-	public static final int 
-	public static final int
-	public static final int
-	public static final int
-	public static final int
-	
-	public static final int
-	public static final int
+	public static final int REEF_FAR_LEFT = 0;
+	public static final int REEF_FAR_CENTER = 1;
+	public static final int REEF_FAR_RIGHT = 2;
+	public static final int REEF_NEAR_LEFT = 3;
+	public static final int REEF_NEAR_CENTER = 4;
+	public static final int REEF_NEAR_RIGHT = 5;
 
-	public static final int
-	public static final int
-	public static final int
-	public static final int
-	public static final int
-	public static final int
-	public static final int
-	public static final int
+	public static final int AUTO_SWITCH = 6;
+
+	public static final int LEVEL_1L = 7;
+	public static final int LEVEL_1R = 8;
+	public static final int LEVEL_2L = 9;
+	public static final int LEVEL_2R = 10;
+	public static final int LEVEL_3L = 11;
+	public static final int LEVEL_3R = 12;
+	public static final int LEVEL_4L = 13;
+	public static final int LEVEL_4R = 14;
+
+
 }
