51a356e862ef1aaff73c8a2d1dfcb7efa7b24af7
Nova Gluchman <yt.nogo721.pro@gmail.com>
2025-02-17 12:42:36 -0600

    Update ButtonBoardConstants.java

--- src/main/java/frc/robot/subsystems/buttonBoard/ButtonBoardConstants.java
+++ src/main/java/frc/robot/subsystems/buttonBoard/ButtonBoardConstants.java
@@ -9,24 +9,27 @@ public class ButtonBoardConstants {
 	private ButtonBoardConstants() {
 	}
 	public static final int BB_PORT = 1;
+
+	public static final int CONNECTION_CHANNEL= 0; //The output from these channels will always be 1, use them to test if we are connected <<<<<<<
+	public static final int DATA_TEST_CHANNEL= 31;
 	
-	public static final int REEF_FAR_LEFT = 0;
-	public static final int REEF_FAR_CENTER = 1;
-	public static final int REEF_FAR_RIGHT = 2;
-	public static final int REEF_NEAR_LEFT = 3;
-	public static final int REEF_NEAR_CENTER = 4;
-	public static final int REEF_NEAR_RIGHT = 5;
+	public static final int REEF_FAR_LEFT = 1;
+	public static final int REEF_FAR_CENTER = 2;
+	public static final int REEF_FAR_RIGHT = 3;
+	public static final int REEF_NEAR_LEFT = 4;
+	public static final int REEF_NEAR_CENTER = 5;
+	public static final int REEF_NEAR_RIGHT = 6;
 
-	public static final int AUTO_SWITCH = 6;
+	public static final int AUTO_SWITCH = 7;
 
-	public static final int LEVEL_1L = 7;
-	public static final int LEVEL_1R = 8;
-	public static final int LEVEL_2L = 9;
-	public static final int LEVEL_2R = 10;
-	public static final int LEVEL_3L = 11;
-	public static final int LEVEL_3R = 12;
-	public static final int LEVEL_4L = 13;
-	public static final int LEVEL_4R = 14;
+	public static final int LEVEL_1L = 8;
+	public static final int LEVEL_1R = 9;
+	public static final int LEVEL_2L = 10;
+	public static final int LEVEL_2R = 11;
+	public static final int LEVEL_3L = 12;
+	public static final int LEVEL_3R = 13;
+	public static final int LEVEL_4L = 14;
+	public static final int LEVEL_4R = 15;
 
 
 }
