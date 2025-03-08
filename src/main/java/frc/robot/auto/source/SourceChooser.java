701b73439a33d74f6acfe75ef6ac1f259d343fe7
Michael M <mmarchiniakjr@hotmail.com>
2025-03-08 11:29:31 -0600

    refactor: simplify source options in SourceChooser and remove unused source locations

--- src/main/java/frc/robot/auto/source/SourceChooser.java
+++ src/main/java/frc/robot/auto/source/SourceChooser.java
@@ -21,12 +21,8 @@ public class SourceChooser {
         // Set default option for the chooser
         sourcePosChooser.setDefaultOption("Auto", SourceLocations.getClosestSource());
         // Add options for different source locations
-        sourcePosChooser.addOption("Left Far", () -> SourceLocations.SOURCE_LEFT_FAR);
-        sourcePosChooser.addOption("Left Middle", () -> SourceLocations.SOURCE_LEFT);
-        sourcePosChooser.addOption("Left Close", () -> SourceLocations.SOURCE_LEFT_CLOSE);
-        sourcePosChooser.addOption("Right Close", () -> SourceLocations.SOURCE_RIGHT_CLOSE);
-        sourcePosChooser.addOption("Right Middle", () -> SourceLocations.SOURCE_RIGHT);
-        sourcePosChooser.addOption("Right Far", () -> SourceLocations.SOURCE_RIGHT_FAR);
+        sourcePosChooser.addOption("Left", () -> SourceLocations.SOURCE_LEFT);
+        sourcePosChooser.addOption("Right", () -> SourceLocations.SOURCE_RIGHT);
         // Put the chooser on the SmartDashboard
         SmartDashboard.putData("Source Pos", sourcePosChooser);
     }
@@ -55,16 +51,6 @@ public class SourceChooser {
         // Define source locations with specific poses
         public static final Pose2d SOURCE_LEFT = new Pose2d(1.121, 7.025, Rotation2d.fromDegrees(-54));
         public static final Pose2d SOURCE_RIGHT = new Pose2d(1.121, 1.027, Rotation2d.fromDegrees(54));
-
-        // Define offset source locations
-        public static final Pose2d SOURCE_LEFT_CLOSE =
-                PoseUtil.offsetPose(SOURCE_LEFT, -0.5, 0); // TODO fill in actual values
-        public static final Pose2d SOURCE_LEFT_FAR =
-                PoseUtil.offsetPose(SOURCE_LEFT, 0.5, 0); // TODO fill in actual values
-        public static final Pose2d SOURCE_RIGHT_CLOSE =
-                PoseUtil.offsetPose(SOURCE_RIGHT, 0.5, 0); // TODO fill in actual values
-        public static final Pose2d SOURCE_RIGHT_FAR =
-                PoseUtil.offsetPose(SOURCE_RIGHT, -0.5, 0); // TODO fill in actual values
 
         /**
          * Gets the closest source to the robot to get coral from.
